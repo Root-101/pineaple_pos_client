9cd324d08877919e56ffc3b53a84cabafefac3c2
Aidyl <jgarciaalbalah@gmail.com>
2022-03-24 15:22:41 -0400

    Exporters: initialized

---
+++ lib/pineaple_exporter.dart
@@ -0,0 +1,7 @@
+library pineaple_exporter;
+
+// Export all the client assets.
+export 'assets/pineaple_assets_exporter.dart';
+
+// Export all the client modules.
+export 'src/ui/pineaple_ui_exporter.dart';
---
+++ lib/src/ui/pineaple_ui_exporter.dart
@@ -0,0 +1,6 @@
+library pineaple_ui_exporter;
+
+// Export all the ui.
+export 'a_module/pineaple_ui_module_exporter.dart';
+export 'b_controller/pineaple_ui_controller_exporter.dart';
+export 'c_controller_impl/pineaple_ui_controller_impl_exporter.dart';
