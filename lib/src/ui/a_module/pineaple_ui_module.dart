71d55ca51f9e8b05baf2a59ae84e2da8a0779d8b
Aidyl <jgarciaalbalah@gmail.com>
2022-03-24 15:18:51 -0400

    PineapleUIModule: initialized

---
+++ lib/src/ui/a_module/pineaple_ui_module.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+import 'package:pineaple_pos_client/pineaple_exporter.dart';
+
+class PineapleUIModule {
+  static const String MODULE_NAME = "Áreas";
+  static Color PRIMARY_COLOR = Colors.blue[800]!;
+  static const Color SECONDARY_COLOR = Colors.blue;
+  static const String URL_MODULE_BACKGROUND = PineapleAssets.AREA_IMAGE;
+  static const IconData ICON = Icons.restaurant_menu_outlined;
+}
