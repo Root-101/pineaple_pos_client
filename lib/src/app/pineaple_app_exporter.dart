ad27878b28b693255eb28c2ac50b2c0ba47bab9b
Aidyl <jgarciaalbalah@gmail.com>
2022-03-31 19:38:49 -0400

    PineapleAppBarWidget: new area text location

--- lib/src/app/pineaple_app_exporter.dart
+++ lib/src/app/pineaple_app_exporter.dart
@@ -1,6 +1,7 @@
 library pineaple_app_exporter;
 
 // Export all the app files.
+export 'a_module/pineaple_module_exporter.dart';
 export 'b_domain/pineaple_domain_exporter.dart';
 export 'c_usecase_def/pineaple_usecase_exporter.dart';
 export 'd_usecase_impl/pineaple_usecase_impl_exporter.dart';
