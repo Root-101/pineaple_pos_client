955a9f8f936716b0c1fb0e6fb7c7841d57a397ee
Aidyl <jgarciaalbalah@gmail.com>
2022-03-31 16:41:22 -0400

    +PineapleAreaTile: + _buildClosed

---
+++ lib/src/app/pineaple_app_exporter.dart
@@ -0,0 +1,6 @@
+library pineaple_app_exporter;
+
+// Export all the app files.
+export 'b_domain/pineaple_domain_exporter.dart';
+export 'c_usecase_def/pineaple_usecase_exporter.dart';
+export 'd_usecase_impl/pineaple_usecase_impl_exporter.dart';
--- lib/src/ui/f_widget/area/pineaple_area_screen_exporter.dart
+++ lib/src/ui/f_widget/area/pineaple_area_screen_exporter.dart
@@ -3,3 +3,4 @@ library pineaple_ui_levels_exporter;
 // Export all the area widgets.
 export 'pineaple_area_screen.dart';
 export 'pineaple_app_bar_widget.dart';
+export 'pineaple_area_tile.dart';
