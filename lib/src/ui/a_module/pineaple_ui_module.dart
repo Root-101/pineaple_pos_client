84926e51f904f737fa812759fad1235a46ae8688
Aidyl <jgarciaalbalah@gmail.com>
2022-03-31 16:40:03 -0400

    PineapleUIModule: colors updated

--- lib/src/ui/a_module/pineaple_ui_module.dart
+++ lib/src/ui/a_module/pineaple_ui_module.dart
@@ -3,8 +3,9 @@ import 'package:pineaple_pos_client/pineaple_exporter.dart';
 
 class PineapleUIModule {
   static const String MODULE_NAME = "Áreas";
-  static Color PRIMARY_COLOR = Colors.blue[800]!;
-  static const Color SECONDARY_COLOR = Colors.blue;
-  static const String URL_MODULE_BACKGROUND = PineapleAssets.AREA_IMAGE;
-  static const IconData ICON = Icons.restaurant_menu_outlined;
+  static Color PRIMARY_COLOR = Color(0xffb4e8f5);
+  static const Color SECONDARY_COLOR = Color(0xffe5cad1);
+  static Color TILE_COLOR = Color(0xff5fb4f7);
+  static const String URL_AREA_BACKGROUND = PineapleAssets.AREA_IMAGE;
+  static const IconData ICON_AREA = Icons.restaurant_menu_outlined;
 }
