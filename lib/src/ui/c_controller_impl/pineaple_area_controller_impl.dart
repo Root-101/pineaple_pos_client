a93f1bc8cf5b2d7a504de8ce9124f5b84dd16328
Aidyl <jgarciaalbalah@gmail.com>
2022-03-31 17:07:42 -0400

    PineapleAreaControllerImpl: - abstract

--- lib/src/ui/c_controller_impl/pineaple_area_controller_impl.dart
+++ lib/src/ui/c_controller_impl/pineaple_area_controller_impl.dart
@@ -1,7 +1,7 @@
 import 'package:get/get.dart';
 import 'package:pineaple_pos_client/pineaple_exporter.dart';
 
-abstract class PineapleAreaControllerImpl extends PineapleAreaController {
+class PineapleAreaControllerImpl extends PineapleAreaController {
   final PineapleAreaUseCase areaUseCase = Get.find<PineapleAreaUseCase>();
 
   @override
