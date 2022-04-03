68d9da302f32775902000a1b7249933c2580ca30
Aidyl <jgarciaalbalah@gmail.com>
2022-04-03 19:16:05 -0400

    Utils folder created

--- lib/src/ui/f_widget/pineaple_ui_widget_exporter.dart
+++ lib/src/ui/f_widget/pineaple_ui_widget_exporter.dart
@@ -3,6 +3,6 @@ library pineaple_ui_widget_exporter;
 // Export all the widgets.
 export 'area/pineaple_ui_area_exporter.dart';
 export 'pos/pineaple_ui_pos_exporter.dart';
-export 'pineaple_app_bar_widget.dart';
-export 'pineaple_refresh_widget.dart';
-export 'pineaple_open_container_widget.dart';
+export 'utils/pineaple_app_bar_widget.dart';
+export 'utils/pineaple_refresh_widget.dart';
+export 'utils/pineaple_open_container_widget.dart';
