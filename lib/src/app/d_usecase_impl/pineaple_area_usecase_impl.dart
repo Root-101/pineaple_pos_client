12c6694858d7277194129638eb92ff3b0c0b8dff
Aidyl <jgarciaalbalah@gmail.com>
2022-04-03 16:34:20 -0400

    PineapleAreaUseCaseImpl: findAll updated

--- lib/src/app/d_usecase_impl/pineaple_area_usecase_impl.dart
+++ lib/src/app/d_usecase_impl/pineaple_area_usecase_impl.dart
@@ -1,4 +1,4 @@
-import 'package:pineaple_pos_client/src/app/b_domain/pineaple_area_domain.dart';
+import 'package:pineaple_pos_client/src/app/b_domain/pineaple_domain_exporter.dart';
 import 'package:pineaple_pos_client/src/app/c_usecase_def/pineaple_area_usecase.dart';
 
 class PineapleAreaUseCaseImpl implements PineapleAreaUseCase {
@@ -34,15 +34,27 @@ class PineapleAreaUseCaseImpl implements PineapleAreaUseCase {
   @override
   List<PineapleAreaDomain> findAll() {
     return [
-      PineapleAreaDomain(id: 1, name: "Mesa 1"),
-      PineapleAreaDomain(id: 2, name: "Mesa 2"),
-      PineapleAreaDomain(id: 3, name: "Mesa 3"),
-      PineapleAreaDomain(id: 4, name: "Mesa 4"),
-      PineapleAreaDomain(id: 5, name: "Mesa 5"),
-      PineapleAreaDomain(id: 6, name: "Mesa 6"),
-      PineapleAreaDomain(id: 7, name: "Mesa 7"),
-      PineapleAreaDomain(id: 8, name: "Mesa 8"),
-      PineapleAreaDomain(id: 9, name: "Barra"),
+      PineapleAreaDomain(id: 1, name: "Lobby", posList: [
+        PineaplePOSDomain(id: 1, name: "Mesa 1"),
+        PineaplePOSDomain(id: 2, name: "Mesa 2"),
+        PineaplePOSDomain(id: 3, name: "Mesa 3"),
+        PineaplePOSDomain(id: 4, name: "Mesa 4"),
+      ]),
+      PineapleAreaDomain(id: 2, name: "Bar", posList: [
+        PineaplePOSDomain(id: 5, name: "Barra"),
+      ]),
+      PineapleAreaDomain(id: 3, name: "Mostrador", posList: [
+        PineaplePOSDomain(id: 6, name: "Mesa 1"),
+        PineaplePOSDomain(id: 7, name: "Mesa 2"),
+      ]),
+      PineapleAreaDomain(id: 4, name: "Platea", posList: [
+        PineaplePOSDomain(id: 8, name: "Mesa 1"),
+        PineaplePOSDomain(id: 9, name: "Mesa 2"),
+        PineaplePOSDomain(id: 10, name: "Mesa 3"),
+        PineaplePOSDomain(id: 11, name: "Mesa 4"),
+        PineaplePOSDomain(id: 12, name: "Mesa 5"),
+        PineaplePOSDomain(id: 13, name: "Mesa 6"),
+      ]),
     ];
   }
 
