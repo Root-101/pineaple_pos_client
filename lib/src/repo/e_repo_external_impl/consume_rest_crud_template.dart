08b0042b6c7b448cb9c0a8288402cc4ae205a0f3
JHB <jhernandezb96@gmail.com>
2022-04-03 18:12:05 -0400

    +ConsumeRESTCRUDTemplate

---
+++ lib/src/repo/e_repo_external_impl/consume_rest_crud_template.dart
@@ -0,0 +1,75 @@
+import 'package:clean_core/clean_core.dart';
+import 'package:http/http.dart' as http;
+
+class ConsumeRESTCRUDTemplate<Entity extends BasicEntityObject>
+    extends CRUDRepositoryExternalAsync<Entity> {
+  final String urlGeneral;
+  final JSONConverter<Entity> converter;
+  final Map<String, String>? _headers = {
+    "Accept": "application/json",
+    "content-type": "application/json"
+  };
+
+  ConsumeRESTCRUDTemplate({required this.urlGeneral, required this.converter});
+
+  @override
+  Future<List<Entity>> findAll() async {
+    String json = await http.read(Uri.parse("$urlGeneral/find_all"));
+    return converter.fromJSONAllString(json);
+  }
+
+  @override
+  Future<Entity> create(Entity newObject) async {
+    String body = converter.toJSONString(newObject);
+    String jsonBody = (await http.post(
+      Uri.parse("$urlGeneral/create"),
+      body: body,
+      headers: _headers,
+    ))
+        .body;
+    return converter.fromJSONString(jsonBody);
+  }
+
+  @override
+  Future<void> destroy(Entity objectToDestroy) async {
+    String body = converter.toJSONString(objectToDestroy);
+    await http.post(
+      Uri.parse("$urlGeneral/destroy"),
+      body: body,
+      headers: _headers,
+    );
+  }
+
+  @override
+  Future<Entity> edit(Entity objectToEdit) async {
+    String body = converter.toJSONString(objectToEdit);
+    String jsonBody = (await http.post(
+      Uri.parse("$urlGeneral/edit"),
+      body: body,
+      headers: _headers,
+    ))
+        .body;
+    return converter.fromJSONString(jsonBody);
+  }
+
+  @override
+  Future<Entity> findBy(int keyId) async {
+    String json = await http.read(Uri.parse("$urlGeneral/find_by/$keyId"));
+    return converter.fromJSONString(json);
+  }
+
+  @override
+  Future<int> count() async {
+    String json = await http.read(Uri.parse("$urlGeneral/count"));
+    return int.parse(json);
+  }
+
+  @override
+  Future<void> destroyById(int id) async {
+    await http.post(
+      Uri.parse("$urlGeneral/destroy_id"),
+      body: "$id",
+      headers: _headers,
+    );
+  }
+}
