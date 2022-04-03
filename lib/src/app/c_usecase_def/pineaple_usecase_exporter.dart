34743614c4ef3358847c8242ee5a23f0ad496048
Aidyl <jgarciaalbalah@gmail.com>
2022-04-03 17:20:59 -0400

    + PineaplePOSUseCase & Impl

--- lib/src/app/c_usecase_def/pineaple_usecase_exporter.dart
+++ lib/src/app/c_usecase_def/pineaple_usecase_exporter.dart
@@ -2,3 +2,4 @@ library pineaple_usecase_exporter;
 
 // Export all the usecase definitions.
 export 'pineaple_area_usecase.dart';
+export 'pineaple_pos_usecase.dart';
---
+++ lib/src/app/d_usecase_impl/pineaple_pos_usecase_impl.dart
@@ -0,0 +1,49 @@
+import 'package:pineaple_pos_client/src/app/pineaple_app_exporter.dart';
+
+class PineaplePOSUseCaseImpl implements PineaplePOSUseCase {
+  @override
+  int count() {
+    // TODO: implement count
+    throw UnimplementedError();
+  }
+
+  @override
+  PineaplePOSDomain create(PineaplePOSDomain newObject) {
+    // TODO: implement create
+    throw UnimplementedError();
+  }
+
+  @override
+  PineaplePOSDomain destroy(PineaplePOSDomain objectToDestroy) {
+    // TODO: implement destroy
+    throw UnimplementedError();
+  }
+
+  @override
+  void dispose() {
+    // TODO: implement dispose
+  }
+
+  @override
+  PineaplePOSDomain edit(PineaplePOSDomain objectToEdit) {
+    // TODO: implement edit
+    throw UnimplementedError();
+  }
+
+  @override
+  List<PineaplePOSDomain> findAll() {
+    // TODO: implement findAll
+    throw UnimplementedError();
+  }
+
+  @override
+  PineaplePOSDomain findBy(int keyId) {
+    // TODO: implement findBy
+    throw UnimplementedError();
+  }
+
+  @override
+  void init() {
+    // TODO: implement init
+  }
+}
